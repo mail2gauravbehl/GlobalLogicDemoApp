097f9ce5615ade5802489294d05dd512d4be6fb9
mail2gauravbehl <mail2gauravbehl@gmail.com>
2016-10-06 21:53:41 +0530

    updated Git Ignore and adding a missing file

---
+++ Services.Tests/MainViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using GlobalLogicDemoApp.Host;
+using GlobalLogigDemoApp.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace Services.Tests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        [Test]
+        public void Test()
+        {
+            Mock<IPostsRepository> repMock = new Mock<IPostsRepository>();
+            repMock.Setup(a => a.GetPosts()).ReturnsAsync(new List<Post>() { new Post() {Body = "body", Id = "1", Title = "title", UserId = "userid"}});
+            var mainViewModel = new  MainViewModel(repMock.Object);
+            mainViewModel.PostsCommand.ExecuteAsync(null);
+            repMock.Verify(mock => mock.GetPosts(), Times.Exactly(1));
+        }
+    }
+}
